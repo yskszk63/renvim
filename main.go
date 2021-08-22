87d97437ef245c621fbeb66130a8bf8702e2bf73
yskszk63 <yskszk63@gmail.com>
2021-08-22 11:02:42 +0900

    refactoring pt2

--- main.go
+++ main.go
@@ -14,39 +14,116 @@ import (
 	"github.com/neovim/go-client/nvim"
 )
 
+func optonly(args []string) bool {
+	for _, a := range args {
+		if !strings.HasPrefix(a, "--") {
+			return false
+		}
+	}
+	return true
+}
+
+func exec_as_nvim(args []string) error {
+	bin, err := exec.LookPath("nvim")
+	if err != nil {
+		return fmt.Errorf("no nvim found: %s\n", err)
+	}
+	newargs := make([]string, len(args)+1)
+	copy(newargs[1:], args)
+	newargs[0] = bin
+
+	env := os.Environ()
+
+	for _, a := range args {
+		if a == "--version" {
+			fmt.Printf("renvim v0.0.0 -- Neovim wrapper.\n\n") // FIXME version
+		}
+	}
+
+	if err := syscall.Exec(bin, newargs, env); err != nil {
+		return fmt.Errorf("failed to exec nvim: %s\n", err)
+	}
+	return nil
+}
+
+func prepare_env(client *nvim.Nvim, opened map[int]bool, wg *sync.WaitGroup) error {
+	if err := client.RegisterHandler("renvimExit", func(buf int) {
+		//fmt.Printf("OK %d %v\n", buf, opened)
+
+		closed, found := opened[buf]
+		if !closed && found {
+			opened[buf] = true
+			wg.Done()
+		}
+	}); err != nil {
+		return fmt.Errorf("failed to register handler: %s", err)
+	}
+	if err := client.Subscribe("renvimExit"); err != nil {
+		return fmt.Errorf("failed to register handler: %s", err)
+	}
+
+	c := fmt.Sprintf(`augroup renvim
+autocmd! BufWinLeave * silent! call rpcnotify(0, "renvimExit", str2nr(expand("<abuf>")))
+augroup END`)
+	if _, err := client.Exec(c, false); err != nil {
+		return fmt.Errorf("failed to regeister autocmd: %s", err)
+	}
+
+	return nil
+}
+
+func tabnew(client *nvim.Nvim, stdin *os.File) (*nvim.Buffer, error) {
+	if err := client.Command("tabnew"); err != nil {
+		return nil, fmt.Errorf("failed to open file: %s", err)
+	}
+
+	buf, err := client.CurrentBuffer()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get current buffer: %s", err)
+	}
+
+	if !isatty.IsTerminal(stdin.Fd()) {
+		sc := bufio.NewScanner(stdin)
+		for sc.Scan() {
+			if err := client.SetBufferLines(buf, -2, -2, false, [][]byte{[]byte(sc.Text())}); err != nil {
+				return nil, fmt.Errorf("failed to get set buffer lines: %s", err)
+			}
+		}
+		if err := sc.Err(); err != nil {
+			return nil, fmt.Errorf("failed to get current buffer: %s", err)
+		}
+	}
+
+	return &buf, nil
+}
+
+func tabnew_with_file(client *nvim.Nvim, file string) (*nvim.Buffer, error) {
+	p, err := filepath.Abs(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to resolve file %s: %s\n", p, err)
+		p = file
+	}
+	command := fmt.Sprintf("tabnew %s", p)
+	if err := client.Command(command); err != nil {
+		return nil, fmt.Errorf("failed to open file %s: %s", p, err)
+	}
+
+	buf, err := client.CurrentBuffer()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get current buffer: %s", err)
+	}
+
+	return &buf, nil
+}
+
 func main() {
 	val, present := os.LookupEnv("NVIM_LISTEN_ADDRESS")
-
 	args := os.Args[1:]
-	optonly := func() bool {
-		for _, a := range args {
-			if !strings.HasPrefix(a, "--") {
-				return false
-			}
-		}
-		return true
-	}()
 
-	if !present || val == "" || (len(args) > 0 && optonly) {
-		bin, err := exec.LookPath("nvim")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "no nvim found: %s\n", err)
+	if !present || val == "" || (len(args) > 0 && optonly(args)) {
+		if err := exec_as_nvim(args); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(-1)
-		}
-		args := make([]string, len(os.Args))
-		copy(args, os.Args)
-		args[0] = bin
-
-		env := os.Environ()
-
-		for _, a := range args[1:] {
-			if a == "--version" {
-				fmt.Printf("renvim v0.0.0 -- Neovim wrapper.\n\n") // FIXME version
-			}
-		}
-
-		if err := syscall.Exec(bin, args, env); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to exec nvim: %s\n", err)
 		}
 	}
 
@@ -62,79 +139,37 @@ func main() {
 	}
 
 	var wg sync.WaitGroup
-	exited := make(map[uint]bool)
-	if err := client.RegisterHandler("renvimExit", func(idx uint) {
-		_, found := exited[idx]
-		if !found {
-			exited[idx] = true
-			wg.Done()
-		}
-	}); err != nil {
-		fmt.Fprintf(os.Stderr, "failed to register handler: %s\n", err)
-		os.Exit(-1)
-	}
-	if err := client.Subscribe("renvimExit"); err != nil {
-		fmt.Fprintf(os.Stderr, "failed to register handler: %s\n", err)
+	opened := make(map[int]bool)
+	if err := prepare_env(client, opened, &wg); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(-1)
 	}
 
-	for idx, a := range args {
+	for _, a := range args {
 		if strings.HasPrefix(a, "--") {
 			fmt.Fprintf(os.Stderr, "Ignore option %s\n", a)
 			continue
 		}
 
 		if a == "-" {
-			if err := client.Command("tabnew"); err != nil {
-				fmt.Fprintf(os.Stderr, "failed to open file: %s\n", err)
+			buf, err := tabnew(client, os.Stdin)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err)
 				os.Exit(-1)
 			}
-
-			if !isatty.IsTerminal(os.Stdin.Fd()) {
-				buf, err := client.CurrentBuffer()
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "failed to get current buffer: %s\n", err)
-					os.Exit(-1)
-				}
-
-				sc := bufio.NewScanner(os.Stdin)
-				for sc.Scan() {
-					if err := client.SetBufferLines(buf, -2, -2, false, [][]byte{[]byte(sc.Text())}); err != nil {
-						fmt.Fprintf(os.Stderr, "failed to get set buffer lines: %s\n", err)
-						os.Exit(-1)
-					}
-				}
-				if err := sc.Err(); err != nil {
-					fmt.Fprintf(os.Stderr, "failed to get current buffer: %s\n", err)
-					os.Exit(-1)
-				}
-			}
+			opened[int(*buf)] = false
+			wg.Add(1)
 
 		} else {
-			p, err := filepath.Abs(a)
+			buf, err := tabnew_with_file(client, a)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "failed to resolve file %s: %s\n", p, err)
-				p = a
+				fmt.Fprintf(os.Stderr, "%s\n", err)
+				os.Exit(-1)
 			}
-			command := fmt.Sprintf("tabnew %s", p)
-			if err := client.Command(command); err != nil {
-				fmt.Fprintf(os.Stderr, "failed to open file %s: %s\n", p, err)
-			}
+			opened[int(*buf)] = false
+			wg.Add(1)
+
 		}
-
-		ch := client.ChannelID()
-		c := fmt.Sprintf(`augroup renvim
-autocmd BufDelete <buffer> silent! call rpcnotify(%[1]d, "renvimExit", %[2]d)
-autocmd BufWinLeave <buffer> silent! call rpcnotify(%[1]d, "renvimExit", %[2]d)
-augroup END
-`, ch, idx)
-
-		if _, err := client.Exec(c, false); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to regeister autocmd: %s", err)
-			os.Exit(-1)
-		}
-
-		wg.Add(1)
 	}
 
 	wg.Wait()
